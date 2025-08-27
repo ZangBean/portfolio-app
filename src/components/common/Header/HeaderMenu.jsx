f90d02836bc74ee513e4a107e2dbeaf41cb09704
zangbin <zangb@GIANG>
2025-08-27 23:43:15 +0700

    fix

--- src/components/common/Header/HeaderMenu.jsx
+++ src/components/common/Header/HeaderMenu.jsx
@@ -1,15 +1,28 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { StyledHeader, Logo, LoginButton } from "./Header.styled.js";
+import AddUserModal from "../Modal/AddUserModal.jsx";
 
 export default function HeaderMenu() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   return (
-    <StyledHeader>
-      <Logo>
-        <Link to="/">Portfolio App</Link>
-      </Logo>
-      <LoginButton type="primary">
-        <Link to="/login">Login</Link>
-      </LoginButton>
-    </StyledHeader>
+    <>
+      <StyledHeader>
+        <Logo>
+          <Link to="/">Portfolio App</Link>
+        </Logo>
+        <LoginButton type="primary" onClick={() => setIsModalVisible(true)}>
+          Thêm User
+        </LoginButton>
+        <LoginButton type="primary">
+          <Link to="/login">Login</Link>
+        </LoginButton>
+      </StyledHeader>
+      <AddUserModal
+        visible={isModalVisible}
+        onClose={() => setIsModalVisible(false)}
+      />
+    </>
   );
 }
