6557a4ef14d6feab0ed95852e7485378b3b70767
Anhthuan25022003 <thuanit25223@gmai.com>
2025-08-28 15:41:14 +0700

    fix home ui

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,14 +12,14 @@ import {
   Title,
 } from './Home.styled'
 import { UserAddOutlined } from '@ant-design/icons'
-import { Spin, List, Button } from 'antd'
+import { List, Button } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { fetchUsersAction } from '../../stores/screens/user/user.action'
 import { setSelectedUser } from '../../stores/screens/user/user.reducer'
 import Loading from '../../components/Loading'
 import AddUserModal from '../../components/common/Modal/AddUserModal'
-import { StyledParagraph } from '../Contact/Contact.styled'
+import ParagraphStyled from '../../components/common/UI/ParagraphStyled'
 
 export default function Home() {
   const dispatch = useDispatch()
@@ -41,19 +41,22 @@ export default function Home() {
   let content
 
   if (status === 'loading') {
-    content = <Loading />
+    return <Loading />
   } else if (status === 'failed') {
-    content = <StyledParagraph>Error: {error}</StyledParagraph>
-  } else {
-    content = (
+    return <ParagraphStyled color='red'>Error: {error}</ParagraphStyled>
+  }
+
+  return (
+    <StyledLayout>
       <StyledList
         grid={{ gutter: 16, column: 3 }}
-        header={<Header>Danh sách người dùng</Header>}
+        header={<Header>Lists User</Header>}
         dataSource={users}
         renderItem={(user) => (
           <List.Item key={user.id}>
             <Link
               to={`/about/${user.id}`}
+              style={{ textDecoration: 'none' }}
               onClick={() => dispatch(setSelectedUser(user))}
             >
               <Card>
@@ -78,50 +81,6 @@ export default function Home() {
           </List.Item>
         )}
       />
-    )
-  }
-
-  return (
-    <StyledLayout>
-      {status === 'loading' ? (
-        <Loading />
-      ) : status === 'failed' ? (
-        <p style={{ color: 'red' }}>Error: {error}</p>
-      ) : (
-        <StyledList
-          grid={{ gutter: 16, column: 3 }}
-          header={<Header>Danh sách người dùng</Header>}
-          dataSource={users}
-          renderItem={(user) => (
-            <List.Item key={user.id}>
-              <Link
-                to={`/about/${user.id}`}
-                style={{ textDecoration: 'none' }}
-                onClick={() => dispatch(setSelectedUser(user))}
-              >
-                <Card>
-                  <CardContent>
-                    {user.cv.personal_info.image ? (
-                      <Avatar src={user.cv.personal_info.image} alt='avatar' />
-                    ) : (
-                      <FallbackAvatar>
-                        {user.cv.personal_info.name[0]}
-                      </FallbackAvatar>
-                    )}
-                    <Info>
-                      <Title>{user.cv.personal_info.name}</Title>
-                      <Description>
-                        <p>Email: {user.cv.personal_info.email || 'N/A'}</p>
-                        <p>Phone: {user.cv.personal_info.phone || 'N/A'}</p>
-                      </Description>
-                    </Info>
-                  </CardContent>
-                </Card>
-              </Link>
-            </List.Item>
-          )}
-        />
-      )}
 
       <Button type='primary' onClick={handleOpenModal}>
         <UserAddOutlined />
